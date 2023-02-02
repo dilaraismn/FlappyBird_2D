2b25acb42f0009d9ecf372600ebffa33e359f844
Dilara <dilaraismn@gmail.com>
2023-02-02 14:48:56 +0300

    All UI Designs Completed

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -1,11 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    private int score;
+    [SerializeField] private TMP_Text scoreText;
 
+    private Player player;
+    private PipeSpawner spawner;
+    public int score { get; private set; }
+
+    private void Awake()
+    {
+        Application.targetFrameRate = 60;
+        
+        player = FindObjectOfType<Player>();
+        spawner = FindObjectOfType<PipeSpawner>();
+
+        Pause();
+    }
+
+    private void Play()
+    {
+        score = 0;
+    }
+
+    private void Pause()
+    {
+        
+    }
     public void IncreaseScore()
     {
         print("increase score");
